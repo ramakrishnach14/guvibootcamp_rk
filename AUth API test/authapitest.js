d57ee1e75933cb9880664d89ee638df92b3e523c
Ch.Rama Krishna <ramakrishnach14@gmail.com>
2020-08-01 13:40:14 +0530

    Update authapitest.js

--- AUth API test/authapitest.js
+++ AUth API test/authapitest.js
@@ -2,7 +2,7 @@
 
 var request = new XMLHttpRequest();
 //by city name
-request.open('GET','https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=019c1fdc421af78df919a03dcd8ca1ab',true);
+request.open('GET','https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=',true);
 request.onload = function(){
 	var data = JSON.parse(this.response);
 	console.log(data);
